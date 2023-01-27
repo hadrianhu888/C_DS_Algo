1965714b3de784cec49b106e13a14fba00b592cc
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-26 21:43:52 -0500

    first commit

---
+++ c_arrays.c
@@ -0,0 +1,42 @@
+/*
+C algorithms and data structures review - Arrays
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void)
+{
+    //array declaration
+    int rollNo[10];
+    
+    //taking inputs 
+    for(int i = 0; i < 10; i++)
+    {
+        printf("Enter roll number: ");
+        scanf("%d", &rollNo[i]);
+    }
+
+    //printing the array
+    for (int i = 0; i < 10; i++) {
+        printf("Roll number: %d\n\n", rollNo[i]);
+    }
+
+    int marks[20]; 
+    int i = 0;
+    int n = 0;
+    int sum = 0;
+    float avg = 0.0;
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        printf("Enter marks of student %d: ", i + 1);
+        scanf("%d", &marks[i]);
+        sum += marks[i];
+    }
+    avg = (float)sum / n;
+    printf("Average marks: %.2f", avg);
+    return 0;
+}
+
---
+++ c_linked_list.c
@@ -0,0 +1,132 @@
+/*
+C linked list example implementation
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+// implement node structure for linked list:
+struct node {
+    int data;
+    struct node *next;
+};
+
+// function to print linked list:
+void insert_end(int);
+void insert_begin(int);
+void insert_after(int, int);
+void delete_end(void);
+void delete_begin(void);
+void delete_after(int);
+void display(void);
+
+int start;
+
+void insert_end(int item) {
+    struct node *temp, *ptr;
+    temp = (struct node *)malloc(sizeof(struct node));
+    if (temp == NULL) {
+        printf("Memory not available\n\n");
+    }
+}
+
+void insert_begin(int item)
+{
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    if (temp == NULL) {
+        printf("Memory not available\n\n");
+    }
+}
+
+void insert_after(int index, int item)
+{
+    struct node *temp, *ptr;
+    temp = (struct node *)malloc(sizeof(struct node));
+    if (temp == NULL) {
+        printf("Memory not available\n\n");
+    }
+}
+
+void delete_end(void)
+{
+    struct node *temp, *ptr;
+    if (start == NULL) {
+        printf("List is empty\n\n");
+    }
+}
+
+void delete_begin(void)
+{
+    struct node *temp;
+    if (start == NULL) {
+        printf("List is empty\n\n");
+    }
+}
+
+void delete_after(int index)
+{
+    struct node *temp, *ptr;
+    if (start == NULL) {
+        printf("List is empty\n\n");
+    }
+}
+
+void display(void)
+{
+    struct node *ptr;
+    if (start == NULL) {
+        printf("List is empty\n\n");
+    }
+}
+
+void main(void)
+{
+    int choice, item, loc;
+    clrscr();
+    while (1) {
+        printf("1. Insert at the end\n2. Insert at the beginning\n3. Insert after a location\n4. Delete from the end\n5. Delete from the beginning\n6. Delete after a location\n7. Display\n8. Exit\n\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+        case 1:
+            printf("Enter the item: ");
+            scanf("%d", &item);
+            insert_end(item);
+            break;
+        case 2:
+            printf("Enter the item: ");
+            scanf("%d", &item);
+            insert_begin(item);
+            break;
+        case 3:
+            printf("Enter the item: ");
+            scanf("%d", &item);
+            printf("Enter the location: ");
+            scanf("%d", &loc);
+            insert_after(item, loc);
+            break;
+        case 4:
+            delete_end();
+            break;
+        case 5:
+            delete_begin();
+            break;
+        case 6:
+            printf("Enter the location: ");
+            scanf("%d", &loc);
+            delete_after(loc);
+            break;
+        case 7:
+            display();
+            break;
+        case 8:
+            exit(0);
+        default:
+            printf("Wrong choice\n\n");
+        }
+    }
+}
+
+// Path: c_linked_list.c 
